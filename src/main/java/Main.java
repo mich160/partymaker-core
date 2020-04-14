58c2ad1dd05e50ecce4a7523a0be036fb73b44ef
lukasz94w <lukasz94w@wp.pl>
2020-04-14 20:31:24 +0200

    First commit - adding class Calendar, Thing, User and some methods to deal with database (searching, adding new users etc. etc.)

---
+++ src/main/java/Main.java
@@ -0,0 +1,52 @@
+import biblioteka.Calendar;
+import model.Thing;
+import model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Calendar c = new Calendar();
+
+//        User user1 = new User("Jan", "Kowalski");
+//        User user2 = new User("John", "Smith");
+//        Thing thing1 = new Thing("Car");
+//
+//        ArrayList<User> usersList = new ArrayList<>();
+//        usersList.add(new User("Johan", "Schmidt"));
+//        usersList.add(new User("Enrico", "Palazzo"));
+//
+//        c.insertUsers(usersList);
+//
+//        ArrayList<Thing> thingArrayList = new ArrayList<>();
+//        thingArrayList.add(new Thing("Plane"));
+//        c.insertThings(thingArrayList);
+//
+//        c.insertUser(user1);
+//        c.insertUser(user2);
+//        c.insertThing(thing1);
+
+
+
+        c.deleteUser(12);
+        c.deleteThing(3);
+        c.getUsers();
+        c.getThings();
+
+        System.out.println(c.getThingCount());
+        System.out.println(c.getUserCount());
+
+        //c.changeThingName(3, "plaaa");
+        //c.findThingByID(3);
+
+        //c.changeUserName(12, "Enriccoa");
+        //c.changeUserSurname(12, "Palllazzoa");
+        //c.findUserByID(12);
+
+        c.closeConnection();
+
+
+    }
+}
---
+++ src/main/java/model/Thing.java
@@ -0,0 +1,17 @@
+package model;
+
+public class Thing {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Thing(String name) {
+        this.name = name;
+    }
+}
